91cae477a95a6e14a386259a7650606a1f4ead38
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-13 22:54:09 +0530

    Initial commit - NEXUS AI Generated

---
+++ gym-website-1773422646218/pages/contact.tsx
@@ -0,0 +1,18 @@
+import { NextPage } from 'next';
+
+const ContactPage: NextPage = () => {
+  return (
+    <div>
+      <h1>Get in Touch</h1>
+      <p>
+        Contact us to learn more about our services and find out how we can
+        help you achieve your fitness goals.
+      </p>
+      <p>Phone: 555-555-5555</p>
+      <p>Email: [info@gymhub.com](mailto:info@gymhub.com)</p>
+      <p>Address: 123 Main St, Anytown, USA</p>
+    </div>
+  );
+};
+
+export default ContactPage;
