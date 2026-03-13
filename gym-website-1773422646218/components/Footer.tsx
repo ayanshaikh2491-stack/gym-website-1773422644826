1069086367960c763aef43cedb1d3550881111c5
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-13 22:54:07 +0530

    Initial commit - NEXUS AI Generated

---
+++ gym-website-1773422646218/components/Footer.tsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>&copy; 2024 GymHub. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
