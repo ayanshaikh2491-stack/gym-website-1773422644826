7e143bdb9f370544f22711e48f4dd1995d50ea7d
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-13 22:54:12 +0530

    Initial commit - NEXUS AI Generated

---
+++ gym-website-1773422646218/lib/utils.ts
@@ -0,0 +1,10 @@
+export function debounce(func: Function, wait: number) {
+  let timeout: number;
+
+  return function (...args: any[]) {
+    clearTimeout(timeout);
+    timeout = setTimeout(() => {
+      func.apply(this, args);
+    }, wait);
+  };
+}
